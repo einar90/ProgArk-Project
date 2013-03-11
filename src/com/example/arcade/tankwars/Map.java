6b1c4579b7dbbf3cac03452f06fd21913fa6e886
Einar Baumann <einar.baumann@gmail.com>
2013-03-11 15:16:00 +0100

    App is now fullscreen

--- src/com/example/arcade/tankwars/Map.java
+++ src/com/example/arcade/tankwars/Map.java
@@ -1,6 +1,7 @@
 package com.example.arcade.tankwars;
 
 import android.graphics.Point;
+import sheep.collision.Polygon;
 
 /**
  * Created by:
@@ -10,12 +11,30 @@ import android.graphics.Point;
  */
 public class Map {
 
-    Point displaySize;
+    private Point displaySize;
+    private Polygon mapPolygon;
 
     public Map(Point displaySize) {
         this.displaySize = displaySize;
-
+        mapPolygon = new Polygon(getMapPointSet());
     }
 
+    public Polygon getMapPolygon() {
+        return mapPolygon;
+    }
 
+    private float[] getMapPointSet() {
+        float[] pointSet = {
+                0, displaySize.y / 6 * 5,
+                displaySize.x / 12 * 2, displaySize.y / 6 * 5,
+                displaySize.x / 12 * 5, displaySize.y / 6 * 4,
+                displaySize.x / 12 * 6, displaySize.y / 6 * 3,
+                displaySize.x / 12 * 7, displaySize.y / 6 * 4,
+                displaySize.x / 12 * 10, displaySize.y / 6 * 5,
+                displaySize.x, displaySize.y / 6 * 5,
+                displaySize.x, displaySize.y,
+                0, displaySize.y
+        };
+        return pointSet;
+    }
 }
