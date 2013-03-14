0a368c287e07b05bf8725c7e415b6e73511c6daa
Einar Baumann <einar.baumann@gmail.com>
2013-03-14 14:46:24 +0100

    Drawing all map elements, but positioning is acting weird

--- src/com/example/arcade/tankwars/UserInterface.java
+++ src/com/example/arcade/tankwars/UserInterface.java
@@ -2,7 +2,6 @@ package com.example.arcade.tankwars;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Point;
 import com.example.arcade.HighscoreList;
 import com.example.arcade.MiniGame;
@@ -27,24 +26,45 @@ public class UserInterface extends State implements MiniGame {
     /**
      * Temp stuff
      */
+    Image mapGroundImage;
+    Image mapPoopImage;
     Sprite mapGround;
+    Sprite mapPoop;
+    float[] scaling;
 
     public UserInterface(Point displaySize) {
-        displaySize = displaySize;
-        mapGround = new Sprite(new Image(R.drawable.map_bottombox));
+        this.displaySize = displaySize;
+        this.scaling = new float[]{displaySize.x / 1280.0f, displaySize.y / 800.0f};
+
+        mapGroundImage = new Image(R.drawable.map_bottombox);
+        mapGround = new Sprite(mapGroundImage);
+        float[] mapGroundSize = new float[]{mapGroundImage.getWidth() * scaling[0], mapGroundImage.getHeight() * scaling[1]};
+        mapGround.setPosition(mapGroundSize[0] / 2, displaySize.y - mapGroundSize[1] / 2);
+        mapGround.setScale(scaling[0], scaling[1]);
+
+        mapPoopImage = new Image(R.drawable.map_poop);
+        mapPoop = new Sprite(mapPoopImage);
+        float[] mapPoopSize = new float[]{mapPoopImage.getWidth() * scaling[0], mapPoopImage.getHeight() * scaling[1]};
+        mapPoop.setPosition(displaySize.x / 2, displaySize.y - mapGroundSize[1]);
+        mapPoop.setScale(scaling[0], scaling[1]);
 
     }
 
 
     public void draw(Canvas canvas) {
+        super.draw(canvas);
 
-        canvas.drawPaint(new Paint(Color.BLUE));
+        canvas.drawColor(Color.BLUE);
+        mapPoop.draw(canvas);
         mapGround.draw(canvas);
 
 
     }
 
     public void update(float dt) {
+
+        mapGround.update(dt);
+        mapPoop.update(dt);
 
     }
 
