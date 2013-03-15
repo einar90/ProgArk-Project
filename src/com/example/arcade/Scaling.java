73b6b5caf7dcb7aa5e95bf16fdc3e329f318dd00
Einar Baumann <einar.baumann@gmail.com>
2013-03-15 11:46:39 +0100

    Moved sprites to assets

--- src/com/example/arcade/Scaling.java
+++ src/com/example/arcade/Scaling.java
@@ -1,10 +1,18 @@
 package com.example.arcade;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
+import sheep.graphics.Image;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by:
@@ -20,4 +28,21 @@ public class Scaling {
             displaySize.x / 1280,
             displaySize.y / 800
     );
+
+    public Image getScaledImage(String filename) {
+        AssetManager assetManager = Game.getInstance().getResources().getAssets();
+        InputStream imageIn;
+        Bitmap bitmapImage;
+        try {
+            imageIn = assetManager.open(filename, AssetManager.ACCESS_BUFFER);
+            bitmapImage = BitmapFactory.decodeStream(imageIn);
+        } catch (IOException e) {
+            Log.e("Error", "Could not open image " + filename);
+        }
+
+        Point originalSize = new Point(bitmapImage.getWidth(), bitmapImage.getHeight());
+
+        bitmapImage = Bitmap.createScaledBitmap(bitmapImage, originalSize.x * scalingFactor.x, originalSize.y * scalingFactor.y);
+
+    }
 }
