0826d594bba15f73e164e370c16f794c6cd6361a
Dzenan Dumpor <dumpor@gmail.com>
2013-03-15 12:33:11 +0100

    Endringer i Tank, gjorde de singleton, og lagte funskjoner for posisjon, reduser ammo, og sjekk ammo

--- src/com/example/arcade/tankwars/Tank.java
+++ src/com/example/arcade/tankwars/Tank.java
@@ -24,9 +24,14 @@ public class Tank extends Sprite {
     private static final Tank tank1 = new Tank(tankImage1);
     private static final Tank tank2 = new Tank(tankImage2);
 
+    private static final Image tankBarrelImage = new Scaling().getScaledImage("tankbarrel");
+    private static Sprite tankBarrel1 = new Sprite(tankBarrelImage);
+    private static Sprite tankBarrel2 = new Sprite(tankBarrelImage);
+
     private int barrelAngle;
     private int power;
     private int hp;
+    private Sprite tankBarrel;
     Projectile projectilePicked;
     private Dictionary projectileAmmo;    //Vet ikke om dictionary fungerer så bra
 
@@ -47,10 +52,25 @@ public class Tank extends Sprite {
         return tank2;
     }
 
+    public static Sprite getTankBarrel1() {
+        tank1.tankBarrel = tankBarrel1;
+        return tank1.tankBarrel;
+    }
+
+    public static Sprite getTankBarrel2() {
+        tank2.tankBarrel = tankBarrel2;
+        return tank2.tankBarrel;
+    }
+
     public static void setInitialTankPositions(Point size) {
         tank1.setPosition(size.x / 10, size.y / 3);
         tank2.setPosition(size.x - size.x / 10, size.y / 3);
         //Setter de litt i løse lufta foreløpig
+        setInitalBarrelPositions(size);
+    }
+
+    public static void setInitalBarrelPositions(Point size) {
+        //sett posisjonen til barrels riktig i forhold til tanksene.
     }
 
     public void reduceHp(int dmg) {
