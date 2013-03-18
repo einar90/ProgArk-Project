867d80048d008d7459eb238a565292ae6dcd1bcb
Christian Thurmann-Nielsen <christian.thurmann@gmail.com>
2013-03-18 15:00:54 +0100

    Added ColdWarModel.

---
+++ src/com/example/arcade/coldWarII/ColdWarModel.java
@@ -0,0 +1,71 @@
+package com.example.arcade.coldWarII;
+
+import android.util.Log;
+import sheep.collision.CollisionListener;
+import sheep.game.Sprite;
+
+public class ColdWarModel implements CollisionListener{
+	private boolean menu,placing;
+	private ColdWarPlayer active, plOne, plTwo;
+	private SnowUnitSpriteContainer plOneCon,plTwoCon;
+	
+	
+	public ColdWarModel(SnowUnitSpriteContainer one,SnowUnitSpriteContainer two){
+		menu = true;
+		placing = false;
+		plOne = new ColdWarPlayer("Christian");
+		plTwo = new ColdWarPlayer("Jørn");
+		active = plOne;
+		plOneCon = one;
+		plTwoCon = two;
+	}
+	public void reverseMenu(){
+		if(menu)
+			menu = false;
+		else
+			menu = true;
+	}
+	public void reversePlacing(){
+		if(placing)
+			placing = false;
+		else
+			placing = true;
+	}
+	public boolean isPlacing(){
+		return placing;
+	}
+	public boolean isMenu(){
+		return menu;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isPlayerOne(){
+		if(active != null && active.equals(plOne))
+			return true;
+		return false;
+	}
+	public void changePlayer(){
+		if(active != null){
+			if(active.equals(plOne))
+				active = plTwo;
+			else
+				active = plOne;
+		}
+	}
+	public void destroySnowUnit(SnowUnitSprite s){
+		if(plOneCon.contains(s))
+			plOneCon.removeSprite(s);
+		else if(plTwoCon.contains(s))
+			plTwoCon.removeSprite(s);
+		s.die();
+	}
+	@Override
+	public void collided(Sprite a, Sprite b) {
+		SnowUnitSprite sa = (SnowUnitSprite) a;
+		SnowUnitSprite sb = (SnowUnitSprite) b;
+		Log.d("Collision!", ""+a.toString()+" collided with "+b.toString());
+		
+	}
+}
