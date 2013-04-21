966166407f660da5c3ea7da258638ac3bf88ef6a
Einar Baumann <einar.baumann@gmail.com>
2013-04-21 19:49:38 +0200

    Added end-game class

---
+++ src/com/example/arcade/tankwars/TankWarsEndGameGUI.java
@@ -0,0 +1,18 @@
+package com.example.arcade.tankwars;
+
+/**
+ * Created by:
+ * User: Einar
+ * Date: 21.04.13
+ * Time: 19:45
+ */
+public class TankWarsEndGameGUI extends sheep.game.State {
+
+    String winnerText;
+    String newGameText = "New game";
+    String toMainMenuText = "Back to main menu";
+
+    public TankWarsEndGameGUI(int winner, int player1Score, int player2Score) {
+
+    }
+}
